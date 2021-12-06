8e43a2f805fca3c54c5610a4bc95e7f75df891b3
sudhanshug6 <sudhanshug6@gmail.com>
2021-12-06 17:53:34 +0530

    new ml code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,11 @@
 print("adding all ml related code here")
+
+
+def model1():
+    print("adding code for model 1")
+
+def model2():
+    print("adding code for model 2")
+
+def model3():
+    print("adding code for model 3")
